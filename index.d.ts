5f09c54d420f3017a81ead5ceffa772749aebb50
Alex Lunyov <isuntc@gmail.com>
2019-01-10 14:01:19 +0800

    Fix typings

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,9 @@
 declare module 'react-file-button' {
   export type FileButtonProps = {
-    renderButton: (value: string, files: FileList | []) => React.ReactNode;
-  };
+    renderButton?: (value: string, files: FileList | []) => any;
+  } & React.HTMLProps<HTMLInputElement>;
 
-  const FileButton: React.FC<
-    FileButtonProps & React.HTMLProps<HTMLInputElement>
-  >;
+  const FileButton: React.FC<FileButtonProps>;
 
   export default FileButton;
 }
